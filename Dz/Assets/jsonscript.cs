a5fd0a3a3986b3a12f6da939b748650e14287836
Aintrex <seva.yakunin.04@Mail.ru>
2024-05-22 16:21:00 +0300

    Dz

---
+++ Dz/Assets/jsonscript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+public class jsonscript : MonoBehaviour
+{
+    public TextMeshProUGUI textMeshPro;
+
+    private string filePath;
+
+    void Start()
+    {
+        filePath = Application.dataPath + "/jason.json";
+    }
+
+    public void LoadTextFromJson()
+    {
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+            Data data = JsonUtility.FromJson<Data>(jsonContent);
+            textMeshPro.text = data.text;
+        }
+    }
+
+    [System.Serializable]
+    public class Data
+    {
+        public string text;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
